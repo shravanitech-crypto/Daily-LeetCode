ec12095843fd75248c25f59463304b5382b1687b
Shravani Sirsat <sms.shravani@gmail.com>
2026-07-30 13:41:31 +0530

    Time: 0 ms (100%), Space: 43.2 MB (47.13%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int sum = 0 ;
+        for (int i = 0 ; i < n ; i++){
+            sum = sum + i/8 + 1;
+
+        }
+        return sum;
+        
+    }
+}
