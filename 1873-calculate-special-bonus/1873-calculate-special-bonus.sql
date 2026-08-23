add00c3a108017014be38efe206074f25aa06d95
Shravani Sirsat <sms.shravani@gmail.com>
2026-08-23 13:21:48 +0530

    Time: 648 ms (56.26%), Space: 0B (100%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT employee_id,
+CASE
+WHEN employee_id % 2 = 0 THEN 0
+WHEN LEFT(name, 1) = 'M' THEN 0
+ELSE salary
+END AS bonus 
+FROM Employees  ORDER BY employee_id ASC  ;
